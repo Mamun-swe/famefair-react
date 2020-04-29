39f6cdc2a917899ab134c6f16d8251c59c05c68f
mamun-swe <mhmamun166009@gmail.com>
2020-04-29 17:35:21 +0600

    okk

--- src/components/admin/master.js
+++ src/components/admin/master.js
@@ -24,11 +24,17 @@ class AdminMaster extends Component {
 
     componentDidMount() {
         $('.dropdown-box').hide()
+        $('.notification_box').hide()
     }
 
     handleSideMenu() {
         $('.side_bar').toggleClass('side_bar_hide', 'slow');
         $('.main_section').toggleClass('side_main_section', 'slow')
+    }
+
+    handleNotification() {
+        // $('.notification_box').slideToggle()
+        $('.notification_box').toggle()
     }
 
     handleDropDown() {
@@ -49,15 +55,62 @@ class AdminMaster extends Component {
                                         </Link>
                                     </div>
                                     <div className="ml-auto">
-                                        <button type="button" className="btn btn-light rounded-circle shadow-none mx-2" onClick={this.handleSideMenu}>
-                                            <i className="fas fa-bars"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-light rounded-circle shadow-none ml-2 mr-3 mr-lg-4 message_btn">
-                                            <i className="fas fa-bell"></i>
-                                            <span className="message_count">10+</span>
-                                        </button>
+                                        <ul>
+                                            <li>
+                                                {/* Side Menu button */}
+                                                <button type="button" className="btn btn-light rounded-circle shadow-none mx-2" onClick={this.handleSideMenu}>
+                                                    <i className="fas fa-bars"></i>
+                                                </button>
+                                            </li>
+                                            <li>
+                                                {/* Notifications Button */}
+                                                <button type="button" className="btn btn-light rounded-circle shadow-none ml-2 mr-3 mr-lg-4 notification_btn" onClick={this.handleNotification}>
+                                                    <i className="fas fa-bell"></i>
+                                                    <span className="notification_count">10+</span>
+                                                </button>
+                                                {/* Notifications Box */}
+                                                <div className="notification_box bg-white shadow-sm border">
+                                                    <div className="tri_angle shadow-sm"></div>
+                                                    <div className="titel_bar px-2 py-1">
+                                                        <p className="mb-0 text-white">Notifications</p>
+                                                    </div>
+                                                    <div className="notification_body">
+                                                        <Link to="/"><p className="mb-0">Lorem ispum 1</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum</p></Link>
+                                                        <Link to="/"><p className="mb-0">Lorem ispum end</p></Link>
+                                                    </div>
+                                                    <div className="notification_footer border-top px-2 py-1">
+                                                        <Link to="">See All</Link>
+                                                    </div>
+                                                </div>
+                                                {/* End Notifications Box */}
+                                            </li>
+                                            <li>
+                                                <div className="user_section">
+                                                    <img src={User} alt="Logged User" className="user_img rounded-circle" />
+                                                    <div className="profile_drop_box bg-white border shadow-sm">
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
 
-                                        <img src={User} alt="Logged User" className="user_img rounded-circle" />
+
+
+
+
                                     </div>
                                 </div>
                             </div>
