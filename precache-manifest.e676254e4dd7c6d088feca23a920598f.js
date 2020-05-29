913544244b642548a0191ad1c5569e9ef89f6030
Mamun-swe <mhmamun166009@gmail.com>
2020-05-29 23:23:20 +0600

    Updates

---
+++ precache-manifest.e676254e4dd7c6d088feca23a920598f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8afcd52ab17578c7d949498b324d86b",
+    "url": "/famefair-react/index.html"
+  },
+  {
+    "revision": "cb58f8652cd4d5f2bc9b",
+    "url": "/famefair-react/static/css/2.331ca8a7.chunk.css"
+  },
+  {
+    "revision": "4e84231a7b551aa2b263",
+    "url": "/famefair-react/static/css/main.e7377ac8.chunk.css"
+  },
+  {
+    "revision": "cb58f8652cd4d5f2bc9b",
+    "url": "/famefair-react/static/js/2.926e7d10.chunk.js"
+  },
+  {
+    "revision": "9e8f0531ef4317a9b68b3232a29209ea",
+    "url": "/famefair-react/static/js/2.926e7d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e84231a7b551aa2b263",
+    "url": "/famefair-react/static/js/main.ab624c04.chunk.js"
+  },
+  {
+    "revision": "97f6140098e6c47021ad",
+    "url": "/famefair-react/static/js/runtime-main.66ac2f67.js"
+  },
+  {
+    "revision": "26db8a7569f37b9229afa3a1ed65bc79",
+    "url": "/famefair-react/static/media/1.26db8a75.png"
+  },
+  {
+    "revision": "7877e514919c42394614296de7cb09b7",
+    "url": "/famefair-react/static/media/10.7877e514.png"
+  },
+  {
+    "revision": "bd252fa662e1c76eece29fb3c3fc0304",
+    "url": "/famefair-react/static/media/2.bd252fa6.png"
+  },
+  {
+    "revision": "81c8660cbfc42c783ff10545c14dc9af",
+    "url": "/famefair-react/static/media/3.81c8660c.png"
+  },
+  {
+    "revision": "4c465dd09b2418f09e64ced0d2bfa66a",
+    "url": "/famefair-react/static/media/4.4c465dd0.png"
+  },
+  {
+    "revision": "54163793843818d459b49bff58b011fa",
+    "url": "/famefair-react/static/media/5.54163793.png"
+  },
+  {
+    "revision": "be22eadf95652eedc3346cc99c68f220",
+    "url": "/famefair-react/static/media/6.be22eadf.png"
+  },
+  {
+    "revision": "ff96f2efeca193617c6cd7b01aa543a7",
+    "url": "/famefair-react/static/media/8.ff96f2ef.png"
+  },
+  {
+    "revision": "20b50f0bff6fdef35a79212dad85e5d8",
+    "url": "/famefair-react/static/media/9.20b50f0b.png"
+  },
+  {
+    "revision": "9a6fb6f5cd3aa4ab1adaaab1f693f266",
+    "url": "/famefair-react/static/media/Rubik-Bold.9a6fb6f5.ttf"
+  },
+  {
+    "revision": "7a06846baf7fd2cfb18c7ab4d663c8ca",
+    "url": "/famefair-react/static/media/Rubik-Light.7a06846b.ttf"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "/famefair-react/static/media/Rubik-Medium.c87313aa.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/famefair-react/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "dc35ea6dfd34e6e9b57ac9bb980dd7a0",
+    "url": "/famefair-react/static/media/offer.dc35ea6d.png"
+  },
+  {
+    "revision": "57e4a2b28dca1e081e8ec1a42da8eb8c",
+    "url": "/famefair-react/static/media/placeholder_img.57e4a2b2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/famefair-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/famefair-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/famefair-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/famefair-react/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6256bebafd4dbf954852a7c7712fc32e",
+    "url": "/famefair-react/static/media/slider1.6256beba.png"
+  },
+  {
+    "revision": "b20d93c38d24f6ade5325ee9006cca3a",
+    "url": "/famefair-react/static/media/slider2.b20d93c3.png"
+  },
+  {
+    "revision": "c9fc4b021652abaf5edfbdbd33ae301d",
+    "url": "/famefair-react/static/media/slider3.c9fc4b02.png"
+  }
+]);
